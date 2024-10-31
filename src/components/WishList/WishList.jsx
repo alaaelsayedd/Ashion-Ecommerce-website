36c7cda0d4bda6f377d46b69a49e75d819b98306
AlaaElsayed <salaaelsayed@gmail.com>
2024-10-31 17:51:59 +0200

    update loadingScreen

--- src/components/WishList/WishList.jsx
+++ src/components/WishList/WishList.jsx
@@ -94,79 +94,78 @@ function WishList() {
   }, []);
   return (
     <>
-      {loadingScreen ? (
+      {loadingScreen && (
         <div
-          className={`fixed top-0 left-0 right-0 bottom-0 bg-gray-100 flex justify-center  items-center text-pink-600 transition   duration-500 ${
-            !isFading && "opacity-0 " 
+          className={`fixed top-0 left-0 right-0 bottom-0 bg-gray-100 flex justify-center z-50  items-center text-pink-600 transition   duration-500 ${
+            !isFading && "opacity-0 "
           }`}
         >
           <Loading />
         </div>
-      ) : (
-        <div className="conatiner cart-cont  w-11/12 mx-auto my-10">
-          <h1 className="text-2xl uppercase  relative head">
-            Your WishList Items{" "}
-          </h1>
-
-          {wishlistproduct.length > 0 ? (
-            <div className="wishlist-cont cards-item grid lg:grid-cols-3 grid-cols-2  px-1 md:px-10 justify-between items-baseline md:gap-10  gap-y-12  sm:gap-y-10 gap-x-4 my-10">
-              {wishlistproduct.map((product, index) => (
-                <WishlistCard
-                  product={product}
-                  deleteProductFromWishlist={ConfirmdeletItemformCart}
-                  addProductToCart={addProductToCart}
-                  isLoading={isLoading}
-                  productId={productId}
-                  key={index}
-                />
-              ))}
-              <Popup trigger={isPopup} closePopup={closePopup} close={closePop}>
-                <h2 className="text-center py-4 font-medium text-lg text-neutral-800">
-                  {" "}
-                  Delete {ProductDetails.title} From Your WishList
-                </h2>
-                <p className="text-center text-neutral-700 text-sm py-1">
-                  Are You Sure?
-                </p>
-                {isLoading && (
-                  <i className="fas fa-spinner fa-spin py-2 text-neutral-900 text-center text-lg block"></i>
-                )}
-                {deleteState.status == "success" ? (
-                  <p className="text-green-600 py-2 text-center">
-                    {deleteState.message}
-                  </p>
-                ) : (
-                  <p className="text-red-600 py-2 text-center">
-                    {deleteState.message}
-                  </p>
-                )}
-
-                <button
-                  className={`text-white bg-red-700 hover:bg-red-800  font-medium rounded-lg text-sm  py-2 w-1/3 block   mx-auto mb-5  text-center ${
-                    isLoading &&
-                    "bg-red-800 bg-opacity-80 cursor-not-allowed hover:bg-opacity-80"
-                  }`}
-                  disabled={isLoading}
-                  onClick={deleteProductFromWishlist}
-                >
-                  Delete
-                </button>
-              </Popup>
-            </div>
-          ) : (
-            <p className="text-2xl mt-10 mb-5 text-center font-semibold">
-              Your WishList Is Empty{" "}
-              <Link
-                className="text-red-700 text-xl underline   rounded-lg  cursor-pointer"
-                to={"/"}
-              >
-                {" "}
-                Add Item to Your WishList{" "}
-              </Link>
-            </p>
-          )}
-        </div>
       )}
+      <div className="conatiner cart-cont  w-11/12 mx-auto my-10">
+        <h1 className="text-2xl uppercase  relative head">
+          Your WishList Items{" "}
+        </h1>
+
+        {wishlistproduct.length > 0 ? (
+          <div className="wishlist-cont cards-item grid lg:grid-cols-3 grid-cols-2  px-1 md:px-10 justify-between items-baseline md:gap-10  gap-y-12  sm:gap-y-10 gap-x-4 my-10">
+            {wishlistproduct.map((product, index) => (
+              <WishlistCard
+                product={product}
+                deleteProductFromWishlist={ConfirmdeletItemformCart}
+                addProductToCart={addProductToCart}
+                isLoading={isLoading}
+                productId={productId}
+                key={index}
+              />
+            ))}
+            <Popup trigger={isPopup} closePopup={closePopup} close={closePop}>
+              <h2 className="text-center py-4 font-medium text-lg text-neutral-800">
+                {" "}
+                Delete {ProductDetails.title} From Your WishList
+              </h2>
+              <p className="text-center text-neutral-700 text-sm py-1">
+                Are You Sure?
+              </p>
+              {isLoading && (
+                <i className="fas fa-spinner fa-spin py-2 text-neutral-900 text-center text-lg block"></i>
+              )}
+              {deleteState.status == "success" ? (
+                <p className="text-green-600 py-2 text-center">
+                  {deleteState.message}
+                </p>
+              ) : (
+                <p className="text-red-600 py-2 text-center">
+                  {deleteState.message}
+                </p>
+              )}
+
+              <button
+                className={`text-white bg-red-700 hover:bg-red-800  font-medium rounded-lg text-sm  py-2 w-1/3 block   mx-auto mb-5  text-center ${
+                  isLoading &&
+                  "bg-red-800 bg-opacity-80 cursor-not-allowed hover:bg-opacity-80"
+                }`}
+                disabled={isLoading}
+                onClick={deleteProductFromWishlist}
+              >
+                Delete
+              </button>
+            </Popup>
+          </div>
+        ) : (
+          <p className="text-2xl mt-10 mb-5 text-center font-semibold">
+            Your WishList Is Empty{" "}
+            <Link
+              className="text-red-700 text-xl underline   rounded-lg  cursor-pointer"
+              to={"/"}
+            >
+              {" "}
+              Add Item to Your WishList{" "}
+            </Link>
+          </p>
+        )}
+      </div>
     </>
   );
 }
